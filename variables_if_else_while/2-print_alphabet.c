f316a0b896dc6460f1526a125fda623d4bee4b18
Spacefox95 <nathan.raynal@gmail.com>
2023-10-16 04:51:11 -0700

    PutcharFor

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Destination
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	char n = 'a';
+	char x = '\n';
+	{
+	for (n = 'a' ; n <= 'z'; n++)
+		putchar(n);
+	}
+	putchar(x);
+
+	return (0);
+}
