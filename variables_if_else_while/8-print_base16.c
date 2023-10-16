3f0b3c9d698688b6942a2344fb101e4902a411b2
Spacefox95 <nathan.raynal@gmail.com>
2023-10-16 06:59:59 -0700

    Hexa

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Destination
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	int n;
+
+	for (n = 48 ; n <= 57 ; n++)
+	{
+		putchar(n);
+	}
+	for (n = 97 ; n <= 102 ; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+	return (0);
+}
