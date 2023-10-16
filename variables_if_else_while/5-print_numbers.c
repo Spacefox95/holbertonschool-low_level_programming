1ed73525ca4b0796fd2cd312b0cea1ed8e428a20
Spacefox95 <nathan.raynal@gmail.com>
2023-10-16 06:50:22 -0700

    Numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Destination
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	int n;
+
+	for (n = 48 ; n <= 57 ; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+	return (0);
+}
