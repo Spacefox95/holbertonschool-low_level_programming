aca915f56c302d0f90d5fdcb6d5063d294a5b48a
Spacefox95 <nathan.raynal@gmail.com>
2023-10-16 06:54:33 -0700

    Alpha

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Destination
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	int n;
+
+	for (n = 122 ; n >= 97 ; n--)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+	return (0);
+}
