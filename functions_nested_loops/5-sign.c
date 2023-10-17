4bfc6d21350b546198f150bc0aff4ad6bc148560
Spacefox95 <nathan.raynal@gmail.com>
2023-10-17 07:57:21 -0700

    sign

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_sign - check number
+ * @n : character to check
+ * Return: If pos = 1, if neg = -1, else =0
+ */
+
+int print_sign(int n)
+{
+	int temp;
+
+		if (n > 0)
+		{
+			_putchar('+');
+			temp = 1;
+		}
+		if (n == 0)
+		{
+			_putchar('0');
+			temp = 0;
+		}
+		else
+		{
+			_putchar('-');
+			temp = -1;
+		}
+		return (temp);
+}
