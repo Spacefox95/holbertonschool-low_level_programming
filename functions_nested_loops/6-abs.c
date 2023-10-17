f633f0852be68d6ee74e5c2c36c850a55abe0205
Spacefox95 <nathan.raynal@gmail.com>
2023-10-17 08:38:01 -0700

    abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - computes the absolute value of integer
+ * @int : character to check
+ * Return: 0 (Success)
+ */
+
+int _abs(int r)
+{
+	_abs( r );
+	return (0);
+}
