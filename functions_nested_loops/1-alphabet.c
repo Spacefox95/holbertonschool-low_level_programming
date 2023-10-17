186a3b9c21ee7012f09bc47d5df180bf6babac84
Spacefox95 <nathan.raynal@gmail.com>
2023-10-17 04:28:59 -0700

    alphaprint

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	int n;
+	{
+		for (n= 97 ; n < 123 ; n++)
+			_putchar(n);
+	}
+	_putchar('\n');
+}
