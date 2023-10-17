c336a5d5064d11afd2682ba9d585241560c344c1
Spacefox95 <nathan.raynal@gmail.com>
2023-10-17 06:03:41 -0700

    x10

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -24,4 +24,3 @@ void print_alphabet_x10(void)
 		_putchar('\n');
 	}
 }
-	
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,2 +1,8 @@
+/**
+ * _putchar - Insert character
+ * @char c : first operand
+ * print_alphabet - Prints alphabet
+ */
 int _putchar(char c);
 void print_alphabet(void);
+void print_alphabet_x10(void);
