62a0a945553328252b1b3c6843d9e4c40858facd
Spacefox95 <nathan.raynal@gmail.com>
2023-10-17 05:01:47 -0700

    alpha

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - check the code
+ * print_alphabet - Prints Alphabet
+ * @print_alphabet - the letter to be checked
  *
- * Return: Always 0.
  */
 
 void print_alphabet(void)
 {
 	int n;
 	{
-		for (n= 97 ; n < 123 ; n++)
+		for (n = 97 ; n < 123 ; n++)
 			_putchar(n);
 	}
 	_putchar('\n');
