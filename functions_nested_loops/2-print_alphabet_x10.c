765543c11c4c7a363f4285f8fd0346b882c2bc34
Spacefox95 <nathan.raynal@gmail.com>
2023-10-17 06:01:32 -0700

    x10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints Alphabet
+ * @print_alphabet_x10 - the letter to be checked
+ *
+ */
+
+void print_alphabet_x10(void)
+
+{
+	char n;
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		n = 'a';
+
+		while (n <= 'z')
+		{
+			_putchar(n);
+			n++;
+		}
+		_putchar('\n');
+	}
+}
+	
