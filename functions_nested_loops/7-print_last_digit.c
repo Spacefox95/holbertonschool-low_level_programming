f54bac39e079ae1996740c2255a6eecee2ac6b96
Spacefox95 <nathan.raynal@gmail.com>
2023-10-18 00:50:42 -0700

    LastDigit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_last_digit - print the last number
+ * @r: character to check
+ * Return: (n) is the answer
+ *
+ */
+
+int print_last_digit(int r)
+{
+	int n;
+
+		if (r < 0)
+	{
+		r = r * -1;
+	}
+
+	n = r % 10;
+	_putchar(n + '0');
+
+	return (n);
+}
