2ab50e761bafcb0d61135c6720273d074b345651
Spacefox95 <nathan.raynal@gmail.com>
2023-10-18 05:08:43 -0700

    Jack

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+ *
+ * 
+ *
+ *
+ *
+ */
+
+void jack_bauer(void)
+
+{
+	int u = 0;
+	int i = 0;
+	
+	while (u < 24)
+	{
+		while (i < 60)
+		{
+			_putchar('0' + (u / 10));
+			_putchar('0' + (u % 10));
+			_putchar(':');
+			_putchar('0' + (i /10));
+			_putchar('0' + (i % 10));
+			_putchar('\n');
+			i++;
+		}
+		i = 0;
+		u++;
+	}
+	
+}
