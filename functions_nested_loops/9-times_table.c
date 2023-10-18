ea9bfdcfc47f3d824f557fce5c9ebe391c3b098d
Spacefox95 <nathan.raynal@gmail.com>
2023-10-18 12:35:04 -0700

    Table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+ * times_table - print multiplication table
+ * @void: ok
+ *
+ */
+
+void times_table(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		for (j = 0 ; j <= 9 ; j++)
+		{
+			k = (i * j);
+				if (j == 0)
+				{
+					_putchar('0' + k);
+				}
+				else
+				{
+
+					_putchar(',');
+					_putchar(' ');
+					if (k < 10)
+					_putchar(' ');
+					_putchar('0' + k);
+				}
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
