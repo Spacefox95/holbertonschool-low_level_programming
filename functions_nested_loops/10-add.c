84f6e3f3fe58cbb01bb1b59007be56ce4739eb05
Spacefox95 <nathan.raynal@gmail.com>
2023-10-19 02:25:22 -0700

    add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+ * add - add two variables
+ * @a: first variable
+ * @b: second variable
+ * Return: result
+ */
+
+int add(int a, int b)
+{
+	int n;
+
+	n = a + b;
+	return (n);
+}
