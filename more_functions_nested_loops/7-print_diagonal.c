06f88b5051a213cd70972975141f64f450869f7c
Spacefox95 <nathan.raynal@gmail.com>
2023-10-20 05:15:13 -0700

    Diag

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include"main.h"
+
+/**
+ * print_diagonal - print character in diag
+ * @n: number of line
+ *
+ */
+
+void print_diagonal(int n)
+{
+	int a;
+	int b;
+
+	if (n > 0)
+	{
+		for (a = 1 ; a <= n ; a++)
+		{
+			for (b = 1 ; b <= a ; b++)
+			{
+				_putchar(32);
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
