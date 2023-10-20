4025c9de28179f23abb8d922fde13cc2f8715780
Spacefox95 <nathan.raynal@gmail.com>
2023-10-20 01:55:43 -0700

    Print

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include"main.h"
+#include<stdio.h>
+
+/**
+ * print_numbers - print numbers in line
+ * @void: ok
+ *
+ */
+
+void print_numbers(void)
+{
+	for (char i = '0' ; i <= '9' ; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -20,5 +20,6 @@ void print_to_98(int n);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
 
 #endif
