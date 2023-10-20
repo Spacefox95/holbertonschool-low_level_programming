2134089860811477ec137a328517dc461502c27e
Spacefox95 <nathan.raynal@gmail.com>
2023-10-20 06:55:44 -0700

    Fizz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -2,9 +2,9 @@
 #include<stdio.h>
 
 /**
- *
- *
- *
+ * main - Print
+ *@void: gg
+ *Return: Success
  */
 
 int main(void)
@@ -15,12 +15,12 @@ int main(void)
 	{
 		if (n % 3 != 0 && n % 5 != 0)
 			printf("%d ", n);
-		if (n % 3 == 0)
-			printf("Fizz ");
-		if (n % 5 == 0)
-			printf("Buzz ");
 		if (n % 3 == 0 && n % 5 == 0)
 			printf("FizzBuzz ");
+		else if (n % 3 == 0)
+			printf("Fizz ");
+		else if (n % 5 == 0)
+			printf("Buzz ");
 	}
 	putchar('\n');
 	return (0);
