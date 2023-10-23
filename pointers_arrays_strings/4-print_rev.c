0c00757b83e37b5d258a730817a1f5d2374337a6
Spacefox95 <nathan.raynal@gmail.com>
2023-10-23 06:51:46 -0700

    Reverse

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,9 +1,28 @@
 #include"main.h"
+#include<stdio.h>
 
-/*
- *
- *
+/**
+ * print_rev - to print string in reverse
+ * @s: variable to display
  *
  */
 
+void print_rev(char *s)
+{
+	int len = 0;
+	int i;
 
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	s--;
+
+	for (i = len ; i > 0 ; i--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
