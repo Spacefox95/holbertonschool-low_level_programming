aa91001e49afe6a031c3cb640a0f931355551128
Spacefox95 <nathan.raynal@gmail.com>
2023-10-24 07:23:43 -0700

    strcopy

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,17 +1,24 @@
 #include"main.h"
 
 /**
- *
- *
+ * _strcpy - function to copy a string
+ * @dest: the destination of the copy
+ * @src: the string that have to be copied
+ * Return: success
  */
 
 char *_strcpy(char *dest, char *src)
 {
-	while (*src)
+	int len = 0;
+	int i;
+
+	while (src[len] != '\0')
 	{
-		*dest = *src;
-		src++;
-		dest++;
+		len++;
+	}
+	for (i = 0 ; i <= len ; i++)
+	{
+		dest[i] = src[i];
 	}
 	return (dest);
 }
