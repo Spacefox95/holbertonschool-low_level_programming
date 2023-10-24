75ed7eaa47cda1a0ecec1327fb9c83e4e4761b7d
Spacefox95 <nathan.raynal@gmail.com>
2023-10-24 10:28:34 -0700

    ts

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -12,7 +12,7 @@ int _atoi(char *s)
 	int sign = 1;
 	int res = 0;
 
-	while (s[i] == ' ' || s[i] == '\t' || s[i] == '+' || s[i] == '-' || (s[i] >= '0' && s[i] <= '9'))
+	while (s[i] == '+' || s[i] == '-' || (s[i] >= '0' && s[i] <= '9'))
 	{
 		if (s[i] == '-')
 			sign = -1;
