cfe66aa4721776acd3974167421e8a46a36d9a7d
Spacefox95 <nathan.raynal@gmail.com>
2023-10-24 01:53:48 -0700

    Puts2

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -13,10 +13,11 @@ void puts2(char *str)
 
 	while (str[i] != '\0')
 	{
-		_putchar(str[i]);
-		i += 2;
-		if (str[i] == '\0')
-			break;
+		if (i % 2 == '\0')
+		{
+			_putchar(str[i]);
+		}
+		i++;
 	}
-		_putchar('\n');
+	_putchar('\n');
 }
