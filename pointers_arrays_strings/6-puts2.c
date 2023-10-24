0f40e992604cabe47b52f160b9e75485c963e843
Spacefox95 <nathan.raynal@gmail.com>
2023-10-24 00:36:52 -0700

    Puts2

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include"main.h"
+#include<stdio.h>
+
+/**
+ * puts2 - print every +2 char
+ * @str: the string to be printed
+ *
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+	_putchar('\n');
+}
