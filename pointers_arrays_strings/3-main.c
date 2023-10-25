888e80f69ad7b347ee116946a87a72f03ddf8eea
Spacefox95 <nathan.raynal@gmail.com>
2023-10-25 08:08:23 -0700

    strcmp

--- pointers_arrays_strings/3-main.c
+++ pointers_arrays_strings/3-main.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-    char s1[] = "Hello";
+char s1[] = "Hello";
     char s2[] = "World!";
 
     printf("%d\n", _strcmp(s1, s2));
--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -16,9 +16,9 @@ int _strcmp(char *s1, char *s2)
 		if (s1[i] != s2[i])
 		{
 			if (s1[i] > s2[i])
-				return (*s1 - *s2);
+				return (s1[i] - s2[i]);
 			if (s1[i] < s2[i])
-				return (*s1 - *s2);
+				return (s1[i] - s2[i]);
 		}
 		i++;
 	}
