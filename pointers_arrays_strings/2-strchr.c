6e0e583d315f73cc74ead87ac4a14c826b7fc3c0
Spacefox95 <nathan.raynal@gmail.com>
2023-10-26 08:24:28 -0700

    strchr

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -10,14 +10,13 @@
 
 char *_strchr(char *s, char c)
 {
-	if (c == '\0')
-		return (s);
+	int i = 0;
 
-	while (*s != '\0')
-	{
-		if (*s == c)
-			return (s);
-		s++;
-	}
-	return ('\0');
+	while (s[i] != '\0' && s[i] != c)
+		i++;
+
+	if (s[i] == c)
+		return (&s[i]);
+	else
+		return ('\0');
 }
