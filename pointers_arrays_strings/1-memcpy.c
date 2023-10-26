aa63d11ae3a3e93adc00bcd9a1cfdf60fd95f1f9
Spacefox95 <nathan.raynal@gmail.com>
2023-10-26 06:56:28 -0700

    memcpy

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -11,12 +11,8 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
-	unsigned int b;
 
-	while (src[b] <= src[n])
-		b++;
-
-	for (i = 0 ; i < b ; i++)
+	for (i = 0 ; i < n ; i++)
 	{
 		dest[i] = src[i];
 	}
