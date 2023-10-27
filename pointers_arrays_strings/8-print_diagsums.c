82e7d7a1bce3fc6e8fd3fdffc2bdb10fee3c7891
Spacefox95 <nathan.raynal@gmail.com>
2023-10-27 06:18:44 -0700

    ok

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -2,12 +2,22 @@
 #include<stdio.h>
 
 /**
- *
- *
- *
+ * print_diagsums - print
+ * @a: ok
+ * @size: ok
  *
  */
 
 void print_diagsums(int *a, int size)
 {
+	int main = 0;
+	int second = 0;
+	int i;
 
+	for (i = 0 ; i < size ; i++)
+	{
+		main += a[i * size + i];
+		second += a[i * size + (size - 1 - i)];
+	}
+	printf("%d, %d\n", main, second);
+}
