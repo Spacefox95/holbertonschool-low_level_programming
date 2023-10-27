a1b1e41168977818144690b91798bb3831750ca6
Spacefox95 <nathan.raynal@gmail.com>
2023-10-27 05:44:12 -0700

    ok

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,17 +1,16 @@
 #include"main.h"
 
 /**
- *
- *
- *
- *
+ * _strpbrk - ok
+ * @s: ok
+ * @accept: ok
+ * Return: success
  */
 
 char *_strpbrk(char *s, char *accept)
 {
 	int i = 0; /* Index for iterating through the input string 's' */
 	int j; /* Index for iterating through the 'accept' string */
-	char *match = '\0'; /* Initialize the match pointer to NULL */
 
 	while (s[i] != '\0') /* Loop through the characters of the input string 's' */
 	{
@@ -20,11 +19,11 @@ char *_strpbrk(char *s, char *accept)
 		{
 			if (s[i] == accept[j])
 			{
-				match = &s[i];/*Set the match pointer and break the loop */
+				return (&s[i]);/*Set the match pointer and break the loop */
 			}
 		j++;
 		}
 	i++;
 	}
-	return (match);
+	return (&s[i]);
 }
---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,13 @@
+#include"main.h"
+#include<stdio.h>
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+void print_diagsums(int *a, int size)
+{
+
