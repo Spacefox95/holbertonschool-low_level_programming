1de38de5e623ba979dd78c2279912759c76fd979
Spacefox95 <nathan.raynal@gmail.com>
2023-10-30 09:40:01 -0700

    perfect

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,9 +1,10 @@
 #include"main.h"
 
 /**
- *
- *
- *
+ * prime_recursive - compare n and d
+ * @n: number to check
+ * @d: divisor of n
+ * Return: Success
  */
 
 int prime_recursive(int n, int d)
@@ -15,11 +16,17 @@ int prime_recursive(int n, int d)
 	return (prime_recursive(n, d + 1));
 }
 
+/**
+ * is_prime_number - check if it's a prime number
+ * @n: number to check
+ * Return: Success
+ */
+
 int is_prime_number(int n)
 {
-	 if (n <= 1)
-        {
-                return (0);
-        }
-        return (prime_recursive(n, 2));
+	if (n <= 1)
+	{
+		return (0);
+	}
+	return (prime_recursive(n, 2));
 }
