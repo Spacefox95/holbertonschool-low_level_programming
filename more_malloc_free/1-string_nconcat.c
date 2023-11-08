a55335c10e2d46f407e6730276872fca77db9b94
Spacefox95 <nathan.raynal@gmail.com>
2023-11-08 05:32:46 -0800

    ok

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -17,6 +17,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int len_2 = 0;
 	char *d;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	
 	while (s1[len_1] != '\0')
 		len_1++;
 	while (s2[len_2] != '\0')
