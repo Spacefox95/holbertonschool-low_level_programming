34318aa48a94309e0d9816797ff09bb4209fa722
Spacefox95 <nathan.raynal@gmail.com>
2023-11-08 05:58:32 -0800

    ok

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -20,13 +20,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
-		s2= "";
+		s2 = "";
 	while (s1[len1] != '\0')
 		len1++;
 	while (s2[len2] != '\0')
 		len2++;
 
 	a = malloc(sizeof(char *) + (len1 + len2 + 1));
+	if (a == NULL)
+		return (NULL);
 
 	for (i = 0 ; i < len1 ; i++)
 		a[i] = s1[i];
@@ -42,8 +44,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			a[len1 + i] = s2[i];
 	}
 
-	if (a == NULL)
-		return (NULL);
 	a[len1 + len2]  = '\0';
 	return (a);
 }
