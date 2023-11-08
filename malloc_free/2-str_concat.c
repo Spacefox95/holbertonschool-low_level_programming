8e512e4777931897ad9e214f3ff8f8c1fe28849b
Spacefox95 <nathan.raynal@gmail.com>
2023-11-08 00:22:46 -0800

    ok

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -17,6 +17,11 @@ char *str_concat(char *s1, char *s2)
 	int len_2 = 0;
 	char *d;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	while (s1[len_1] != '\0')
 		len_1++;
 	while (s2[len_2] != '\0')
