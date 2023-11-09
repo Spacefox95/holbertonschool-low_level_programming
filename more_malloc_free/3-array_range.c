60ad177fd27a75be85221a48d23216eb5a8c92f0
Spacefox95 <nathan.raynal@gmail.com>
2023-11-09 05:27:22 -0800

    ok

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -22,7 +22,7 @@ int *array_range(int min, int max)
 	if (a == NULL)
 		return (NULL);
 
-	for (i = 0 ; i <= max - min ; i++)
+	for (i = 0 ; i <= max - min + 1 ; i++)
 		a[i] = min + i;
 
 	return (a);
