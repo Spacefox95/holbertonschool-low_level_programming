5b59fe42b5091223a6aa6d13d95d4a4fb2cf05b5
Spacefox95 <nathan.raynal@gmail.com>
2023-11-13 03:26:15 -0800

    ko

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -11,12 +11,12 @@
  * @owner: ok
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
