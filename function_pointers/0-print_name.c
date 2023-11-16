aabe5fb28db4f8859a9a94597e88703a722294c8
Spacefox95 <nathan.raynal@gmail.com>
2023-11-16 00:34:49 -0800

    ok

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,3 +1,4 @@
+#include<stddef>
 #include"function_pointers.h"
 
 /**
@@ -8,7 +9,6 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL && f == NULL)
-		return;
-	f(name);
+	if (name != NULL && f != NULL)
+		f(name);
 }
