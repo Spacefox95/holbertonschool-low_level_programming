13969579cdd3ad33d02eb0490c29dc8833fb668b
Spacefox95 <nathan.raynal@gmail.com>
2023-11-17 14:23:20 -0800

    ok

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,16 +1,23 @@
-#ifndef VARIADIC_FUNCTION
-#define VARIADIC_FUNCTION
+#ifndef VARIADIC_FUNCTIONS
+#define VARIADIC_FUNCTIONS
 
-typedef struct dt
-	{
-	    char letter;
-	    void (*func)(va_list);
-	} datatype;
+#include <stdarg.h>
 
-
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+/**
+ * struct dt - struct
+ * @letter: letter signifying data type
+ * @func: function ptr
+ */
+typedef struct dt
+{
+	char letter;
+	void (*func)(va_list);
+} datatype;
 
 #endif
