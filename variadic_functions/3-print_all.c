b6d7b154b0ff37ffe8500132de1244ba022398f3
Spacefox95 <nathan.raynal@gmail.com>
2023-11-17 11:30:32 -0800

    ok

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,57 +1,72 @@
-#include "variadic_functions.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include "variadic_functions.h"
 
-/**
- * print_all - print everything
- * @format: an array of chars
- */
+void print_char(va_list args)
+{
+	printf("%c", va_arg(args, int));
+}
+void print_int(va_list args)
+{
+	printf("%d", va_arg(args, int));
+}
+void print_float(va_list args)
+{
+	printf("%f", va_arg(args, double));
+}
+void print_string(va_list args)
+{
+	char *x;
+	x = va_arg(args, char *);
+	if (x == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", x);
+}
 
+typedef struct op_t
+{
+	char character;
+	void (*f)(va_list);
+} type;
 void print_all(const char * const format, ...)
 {
+	char *space = "";
+	int i, j = 0;
 	va_list args;
-	const char *x = format;
-	char *j = " ";
 
+	type ops[]={
+		{'c', print_char},
+		{'d', print_int},
+		{'f', print_float},
+		{'s', print_string},
+		{'\0', NULL}
+	};
+
+	if (format == NULL)
+	{
+		printf("\n");
+		return;
+	}
 	va_start(args, format);
 
-	while (*x)
+	while (format[j] != '\0')
 	{
-		switch (*x)
+		i = 0;
+		while(ops[i].character != '\0')
 		{
-			case 'c':
-				{
-					char c = va_arg(args, int);
-					printf("%c", c);
-					break;
-				}
-			case 'i':
-				{
-					int i = va_arg(args, int);
-					printf("%d", i);
-					break;
-				}
-			case 'f':
-				{
-					double f = va_arg(args, double);
-					printf("%f", f);
-					break;
-				}
-			case 's':
-				{
-					char *s = va_arg(args, char *);
-					printf("%s", (s != NULL) ? s : "(nil)");
-					break;
-				}
-			default:
-				break;
+			if (ops[i].character == format[j])
+			{
+				printf("%s", space);
+				ops[i].f(args);
+				space = ", ";
+			}
+			i++;
 		}
-		x++;
-		if (*j)
-		{
-			printf(", ");
-		}
+		j++;
 	}
 	va_end(args);
 	printf("\n");
