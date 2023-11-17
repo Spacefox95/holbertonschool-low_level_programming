484b150fee0095c68bf77b9fb1a063742c3a2ae9
Spacefox95 <nathan.raynal@gmail.com>
2023-11-17 14:20:24 -0800

    ok

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -3,71 +3,84 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
 
-void print_char(va_list args)
+/**
+ * print_char - prints char
+ * @valist: valist
+ */
+void print_char(va_list valist)
 {
-	printf("%c", va_arg(args, int));
+	printf("%c", va_arg(valist, int));
 }
-void print_int(va_list args)
+
+/**
+ * print_int - prints int
+ * @valist: valist
+ */
+void print_int(va_list valist)
 {
-	printf("%d", va_arg(args, int));
+	printf("%d", va_arg(valist, int));
 }
-void print_float(va_list args)
+
+/**
+ * print_float - prints float
+ * @valist: valist
+ */
+void print_float(va_list valist)
 {
-	printf("%f", va_arg(args, double));
+	printf("%f", va_arg(valist, double));
 }
-void print_string(va_list args)
+
+/**
+ * print_string - prints string
+ * @valist: valist
+ */
+void print_string(va_list valist)
 {
-	char *x;
-	x = va_arg(args, char *);
-	if (x == NULL)
+	char *s;
+
+	s = va_arg(valist, char *);
+
+	if (s == NULL)
 	{
 		printf("(nil)");
 		return;
 	}
-	printf("%s", x);
+	printf("%s", s);
 }
 
-typedef struct op_t
-{
-	char character;
-	void (*f)(va_list);
-} type;
+/**
+ * print_all - print varying input of ints, chars, floats, and strings
+ * @format: an array of chars signifying which data type to print
+ */
 void print_all(const char * const format, ...)
 {
-	char *space = "";
+	char *separator = "";
 	int i, j = 0;
-	va_list args;
+	va_list valist;
 
-	type ops[]={
-		{'c', print_char},
-		{'d', print_int},
-		{'f', print_float},
-		{'s', print_string},
-		{'\0', NULL}
-	};
+	datatype choice[] = { {'c', print_char},
+			      {'i', print_int},
+			      {'f', print_float},
+			      {'s', print_string},
+			      {'\0', NULL} };
 
-	if (format == NULL)
-	{
-		printf("\n");
-		return;
-	}
-	va_start(args, format);
-
-	while (format[j] != '\0')
+	/* iterate format; if datatype matched, access function via struct */
+	va_start(valist, format);
+	while (format != NULL && format[j] != '\0')
 	{
 		i = 0;
-		while(ops[i].character != '\0')
+		while (choice[i].letter != '\0')
 		{
-			if (ops[i].character == format[j])
+			if (choice[i].letter == format[j])
 			{
-				printf("%s", space);
-				ops[i].f(args);
-				space = ", ";
+				printf("%s", separator);
+				choice[i].func(valist); /*access va_arg later*/
+				separator = ", ";
 			}
 			i++;
 		}
 		j++;
 	}
-	va_end(args);
+	va_end(valist);
 	printf("\n");
 }
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,6 +1,13 @@
 #ifndef VARIADIC_FUNCTION
 #define VARIADIC_FUNCTION
 
+typedef struct dt
+	{
+	    char letter;
+	    void (*func)(va_list);
+	} datatype;
+
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
