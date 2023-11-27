0085b34bb7bacf6c4ddfb0975ba00f457e02599f
Spacefox95 <nathan.raynal@gmail.com>
2023-11-27 06:00:10 -0800

    Add_node_end

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -14,12 +14,15 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *new;
 	list_t *last_node;
 
-	new = malloc(sizeof(list_t));
-
 	if (str == NULL)
 		return (NULL);
 
-	while(str[length])
+	new = malloc(sizeof(list_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	while (str[length])
 		length++;
 
 	new->str = strdup(str);
