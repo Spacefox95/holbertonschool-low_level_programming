6b51efebdc848c9fb162be357bc56b972f28d634
Spacefox95 <nathan.raynal@gmail.com>
2023-11-28 00:52:26 -0800

    Doublelinkedlist

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_dlisint - function that print a double linked list
+ * @h - elements of the list
+ * Return: number of nodes of the list
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
