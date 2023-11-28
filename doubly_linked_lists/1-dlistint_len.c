1fda0cd586a182645a94441361e02144987dce48
Spacefox95 <nathan.raynal@gmail.com>
2023-11-28 00:55:59 -0800

    Task 1

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ *
+ *
+ *
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
