d7f54ad20ce8994e3f215060104b15907396b2c0
Spacefox95 <nathan.raynal@gmail.com>
2023-11-30 05:28:17 -0800

    Task7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -29,7 +29,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		new_node->next = *h;
 		if (*h != NULL)
 			(*h)->prev = new_node;
-		*h = new_node;
+		(*h) = new_node;
 		return (new_node);
 	}
 	for (i = 0 ; i < idx - 1 && current_node != NULL; i++)
