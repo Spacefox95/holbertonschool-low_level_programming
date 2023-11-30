2a3e7c451038e5a85a7cd59032216b40bba01744
Spacefox95 <nathan.raynal@gmail.com>
2023-11-30 05:11:49 -0800

    Task7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -18,36 +18,27 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 
 	new_node = malloc(sizeof(dlistint_t));
-
 	if (new_node == NULL)
 		return (NULL);
-
 	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = NULL;
-
 	if (idx == 'O')
 	{
 		new_node->next = *h;
 		if (*h != NULL)
-		{
 			(*h)->prev = new_node;
-		}
 		*h = new_node;
 		return (new_node);
 	}
 	current_node = *h;
-
-	for (i = 0 ; i < idx - 1 ; i++)
-	{
+	for (i = 0 ; i < idx - 1 && current_node != NULL; i++)
 		current_node = current_node->next;
-	}
 	if (current_node == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
-
 	new_node->next = current_node->next;
 	new_node->prev = current_node;
 	if (current_node->next != NULL)
