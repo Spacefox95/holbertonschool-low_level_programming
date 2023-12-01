c92fcfccc97c5908a81b785a43b3578a3f502979
Spacefox95 <nathan.raynal@gmail.com>
2023-12-01 02:30:08 -0800

    ok

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * set_bit - set the value of a bit to 1 at a given position
+ * @n: value to set
+ * @index: position of the bit
+ * Return: 1 of it worked, -1 if failed
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+	
+	*n = *n | (1UL << index);
+	return (1);
+}
