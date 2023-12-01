26bbde13546fc18d8ddd32aa28d7f28323d9ab31
Spacefox95 <nathan.raynal@gmail.com>
2023-12-01 03:02:49 -0800

    ok

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * flip_bits - return number of bit needed to 
+ * flip from one number to another
+ * @n: first number
+ * @m: second number
+ * Return: number of flipping bits
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int result = n ^ m;
+	unsigned int count = 0;
+
+	while (result > 0)
+	{
+		count += result & 1;
+		result >>= 1;
+	}
+	return (count);
+}
