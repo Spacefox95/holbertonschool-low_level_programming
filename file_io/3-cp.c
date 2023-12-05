c6296e23b276979778dec6ed0e91f0f5b8e88f2c
Spacefox95 <nathan.raynal@gmail.com>
2023-12-05 00:48:18 -0800

    ok

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -2,15 +2,18 @@
 #include <stdlib.h>
 #include "main.h"
 
+#define BUFFER_SIZE 1024
+
 /**
- * main - check the code
- *
+ * main - function to copy the content of a file to another file
+ * @argc: argument coutner
+ * @argv: argument value
  * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
-	char *buffer;
-	int file, file2;
+	char buffer[BUFFER_SIZE];
+	int file_from, file_to;
 	ssize_t bytesRead, bytesWritten;
 
 	if (argc != 3)
@@ -18,48 +21,38 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp %s %s\n", argv[1], argv[2]);
 		exit(97);
 	}
-	file = open(argv[1], O_RDONLY);
-	if (file == -1)
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	file2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR, S_IRGRP, S_IWGRP | S_IROTH);
-	if (file2 == -1)
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	if (file_to == -1)
 	{
-		close(file);
+		close(file_from);
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-	buffer = (char *)malloc(1024);
-	if (buffer == NULL)
+	while ((bytesRead = read(file_from, buffer, BUFFER_SIZE)) > 0)
 	{
-		close(file);
-		close(file2);
-		return (0);
-	}
-	while ((bytesRead = read(file, buffer, 1024)) > 0)
-	{
-		bytesWritten = write(file2, buffer, bytesRead);
+		bytesWritten = write(file_to, buffer, bytesRead);
 		if (bytesRead != bytesWritten)
 		{
-			close(file);
-			close(file2);
-			free(buffer);
+			close(file_from);
+			close(file_to);
 			return (0);
 		}
 	}
-	if (close(file) == -1)
+	if (close(file_from) == -1)
 	{
-		dprintf(STDERR_FILENO, "Can't close fd %d\n", file);
+		dprintf(STDERR_FILENO, "Can't close fd %d\n", file_from);
 		exit(100);
 	}
-	if (close(file2) == -1)
+	if (close(file_to) == -1)
 	{
-		dprintf(STDERR_FILENO, "Can't close fd %d\n", file2);
+		dprintf(STDERR_FILENO, "Can't close fd %d\n", file_to);
 		exit(100);
 	}
-	close(file);
-	close(file2);
 	return (0);
 }
