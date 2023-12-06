93fff43abaefe0492437a59397fdb65b2c3c45c0
Spacefox95 <nathan.raynal@gmail.com>
2023-12-06 01:41:47 -0800

    ok

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -10,10 +10,10 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
-	
+
 	if (size == '\0')
 		return (NULL);
-	
+
 	table = malloc(sizeof(hash_table_t));
 
 	if (table == NULL)
