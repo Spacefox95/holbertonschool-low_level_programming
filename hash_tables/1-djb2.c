04172f20e5beb92a7e87f520a4d5dc730ccea046
Spacefox95 <nathan.raynal@gmail.com>
2023-12-06 04:18:30 -0800

    ok

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -1,3 +1,12 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - implement djb2 algorithm
+ * @str: value
+ * Return: key
+ */
+
+
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
