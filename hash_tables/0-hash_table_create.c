494ef9b9e58702169b8d46aa95e477079008490c
Spacefox95 <nathan.raynal@gmail.com>
2023-12-06 02:34:17 -0800

    ok

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -12,24 +12,22 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *table;
 	unsigned long int i;
 
-	if (size == '\0')
-		return (NULL);
-
-	table = malloc(sizeof(hash_table_t));
-
-	if (table == NULL)
-		return (NULL);
-	table->size = size;
-
-	table->array = (hash_node_t **)malloc(sizeof(hash_node_t *) * size);
-	if (table->array == NULL)
+	if (size > 0)
 	{
-		free(table);
-		return (NULL);
+
+		table = malloc(sizeof(hash_table_t));
+		if (table == NULL)
+			return (NULL);
+		table->size = size;
+		table->array = (hash_node_t **)malloc(sizeof(hash_node_t *) * size);
+		if (table->array == NULL)
+		{
+			free(table);
+			return (NULL);
+		}
+		for (i = 0 ; i < size ; i++)
+			table->array[i] = NULL;
+		return (table);
 	}
-
-	for (i = 0 ; i < size ; i++)
-		table->array[i] = NULL;
-
-	return (table);
+	return (NULL);
 }
