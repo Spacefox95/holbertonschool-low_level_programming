e6f1af83785cee8f62da384abb1994cd65aa78df
Spacefox95 <nathan.raynal@gmail.com>
2023-12-07 05:47:18 -0800

    ok

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -9,21 +9,22 @@ void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *current;
 	unsigned long int index;
+	char *comma = "";
 
 	if (ht == NULL)
 		return;
 
 	printf("{");
-	for (index = 0 ; index < (ht->size - 1) ; index++)
+	for (index = 0 ; index < ht->size - 1 ; index++)
 	{
 		current = ht->array[index];
 		while (current != NULL)
 		{
-			printf("'%s': '%s'", current->key, current->value);
-			if (current->next == NULL)
-				printf(", ");
+			printf("%s'%s': '%s'", comma, current->key, current->value);
+			comma = ", ";
 			current = current->next;
 		}
+
 	}
 	printf("}\n");
 }
