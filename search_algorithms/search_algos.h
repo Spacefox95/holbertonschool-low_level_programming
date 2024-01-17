f2af954551f106e90f608869e7b2f90b439eb513
Spacefox95 <nathan.raynal@gmail.com>
2024-01-17 11:26:13 +0100

    Update.h

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -6,8 +6,11 @@
 #include <stdlib.h>
 #include <stddef.h>
 #include <sys/types.h>
+#include <math.h>
 
 
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+void print_array(int *array, size_t i, size_t j);
 
 #endif
